07295f8326a327fd6314a8c7d3c62b6f18e4cd68
davejonesbkk <davejonesbkk@gmail.com>
2018-04-09 17:03:54 +0100

    Updated virtualenv path

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -3,7 +3,7 @@
 ssh ubuntu@35.173.136.101 <<EOF
   cd djtrump
   git pull
-  source /opt/envs/djtrump/bin/activate
+  source /djtrumpenv/bin/activate
   pip install -r requirements.txt
   ./manage.py migrate
   sudo supervisorctl restart djtrump
